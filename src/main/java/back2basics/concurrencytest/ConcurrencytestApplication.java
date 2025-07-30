d214155e66a228635deb273322090a840ff7c967
swlee <swo98@naver.com>
2025-07-30 20:17:31 +0900

    init: project setting

---
+++ src/main/java/back2basics/concurrencytest/ConcurrencytestApplication.java
@@ -0,0 +1,13 @@
+package back2basics.concurrencytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencytestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/back2basics/concurrencytest/ConcurrencytestApplicationTests.java
@@ -0,0 +1,13 @@
+package back2basics.concurrencytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencytestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
