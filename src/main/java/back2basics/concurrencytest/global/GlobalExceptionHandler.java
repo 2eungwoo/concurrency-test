e753be85b13856dc03a33feb70745fb7b307befb
swlee <swo98@naver.com>
2025-07-31 00:28:17 +0900

    fix: 각 커스텀 익셉션을 잡도록 핸들러 수정. 상태코드는 그냥 하나로 통일

--- src/main/java/back2basics/concurrencytest/global/GlobalExceptionHandler.java
+++ src/main/java/back2basics/concurrencytest/global/GlobalExceptionHandler.java
@@ -2,12 +2,28 @@ package back2basics.concurrencytest.global;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
+import back2basics.concurrencytest.common.exception.EventClosedException;
+import back2basics.concurrencytest.common.exception.OutOfStockException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EventClosedException.class)
+    protected ResponseEntity<ApiResponse> handleEventClosedException(Exception e) {
+        return ResponseEntity
+            .status(INTERNAL_SERVER_ERROR)
+            .body(ApiResponse.fail(e.getMessage()));
+    }
+
+    @ExceptionHandler(OutOfStockException.class)
+    protected ResponseEntity<ApiResponse> handleOutOfStockException(Exception e) {
+        return ResponseEntity
+            .status(INTERNAL_SERVER_ERROR)
+            .body(ApiResponse.fail(e.getMessage()));
+    }
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ApiResponse> handleException(Exception e) {
