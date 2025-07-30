183d9ccf25cc5f664dba5c525670a9b1a86924e0
swlee <swo98@naver.com>
2025-07-30 20:59:15 +0900

    fix: 커피리워드 enum 클래스 if-else 너무 못생겨서 리팩토링

--- src/main/java/back2basics/concurrencytest/domain/coffee/reward/CoffeeRewardPolicy.java
+++ src/main/java/back2basics/concurrencytest/domain/coffee/reward/CoffeeRewardPolicy.java
@@ -3,18 +3,11 @@ package back2basics.concurrencytest.domain.coffee.reward;
 public class CoffeeRewardPolicy {
 
     public static CoffeeReward getRewardByRank(int rank) {
-        if (rank == 1) {
-            return CoffeeReward.GOLD;
-        } else if (rank == 2) {
-            return CoffeeReward.SILVER;
-        } else if (rank == 3) {
-            return CoffeeReward.BRONZE;
-        } else if (rank >= 4 && rank <= 10) {
-            return CoffeeReward.WOOD;
-        } else if (rank >= 11 && rank <= 100) {
-            return CoffeeReward.NORMAL;
-        } else {
-            throw new IllegalArgumentException("늦었습니다. 오소이! : " + rank);
-        }
+        if (rank == 1) return CoffeeReward.GOLD;
+        if (rank == 2) return CoffeeReward.SILVER;
+        if (rank == 3) return CoffeeReward.BRONZE;
+        if (rank <= 10) return CoffeeReward.WOOD;
+        if (rank <= 100) return CoffeeReward.NORMAL;
+        throw new IllegalArgumentException("늦었습니다. 오소이! : " + rank);
     }
 }
